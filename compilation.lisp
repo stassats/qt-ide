531dbefa0f2528f1680e735615a9fbc459d6454e
Stas Boukarev <stassats@gmail.com>
2013-11-10 20:44:26 +0400

    Do not use mkstemp on windows.

--- compilation.lisp
+++ compilation.lisp
@@ -6,7 +6,11 @@
 (in-package #:qt-ide)
 
 (defun make-temporary-file ()
-  (sb-posix:mkstemp "/tmp/compilation.lispXXXXXX"))
+  #+(and sbcl (not win32))
+  (sb-posix:mkstemp "/tmp/compilation.lispXXXXXX")
+  #-(and sbcl (not win32))
+  (uiop/stream:with-temporary-file (:keep t :pathname pathname)
+    pathname))
 
 (defun compile-form (form)
   (let ((temp (make-temporary-file)))
